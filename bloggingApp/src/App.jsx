3fae4c3957a2c6acf4293bcb41bd8bcf226737f9
sankalpramteke <116703076+sankalpramteke@users.noreply.github.com>
2025-02-18 19:09:15 +0530

    none

---
+++ bloggingApp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(Process.env.REACT_APP_APPWRITE_URL);
+  
+
+  return (
+    <>
+      <h1>A Blog app in appwrite</h1>
+    </>
+  )
+}
+
+export default App
